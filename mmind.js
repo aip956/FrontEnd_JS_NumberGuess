2d54a9995c11a7c870f11ef77d918df7026fde1c
Anthea Ip <antheaip@Antheas-MBP.lan>
2023-04-29 18:27:46 -0400

    Added some styling; toggle not working

--- mmind.js
+++ mmind.js
@@ -1,4 +1,3 @@
-// from working node file
 
 ///////////// VARIABLES /////////////////
 let secret = '';
@@ -8,13 +7,17 @@ let round_num = 1;
 let you_won = 0;
 let max_tries = 10;
 
+
 ///////////// HTML ELEMENTS /////////////////
-// Submit listener
+
+const gridThree = document.getElementById("grid-con-3");
+const gridFour = document.getElementById("grid-con-4");
 const guessForm = document.getElementById("guess-form");
 const roundElem = document.getElementById("round-num");
 const guessInput = document.getElementById("guess");
 const misPlaced = document.getElementById("mis-placed");
 const wellPlaced = document.getElementById("well-placed");
+// const youWon = document.getElementById("you-won");
 
 ///////////// LISTENER /////////////////
 guessForm.addEventListener("submit", function(event) {
@@ -36,18 +39,13 @@ function make_secret() {
     while (i < 4) {
         // Prevent repeating numbers
         let randomNumber = Math.floor(Math.random() * 8);
-        // console.log("13RanNum: ", randomNumber)
-        // console.log("14Arr[ran]: ", nonrept_array[randomNumber])
         if (nonrept_array[randomNumber] == 0) {
             nonrept_array[randomNumber] ++
-            // console.log("Nonrept_arr: ", i, nonrept_array)
             randomChar = randomNumber.toString();
             secret = secret.concat(randomChar);
             i++
         }   
     }
-    // console.log("18Secret in secret fn: ", secret);
-    // return secret
 }
 
 // Create count array for secret
@@ -57,7 +55,6 @@ function secret_counts(){
         secr_count_array[secret[i]] ++
     }
     // console.log("28Sec_arr in sec_counts: ", secr_count_array);
-    // return secr_count_array
 }
 
 // Validate guess is 4 digits between 0 -7
@@ -86,7 +83,6 @@ function guess_counts(guess) {
     let guess_count_array = [0, 0, 0, 0, 0, 0, 0, 0];
     console.log("60Guess in guess_cts: ", guess);
     for (i = 0; i < 4; i++) {
-        // console.log("guess[i]: ", guess[i]);
         guess_count_array[guess[i]] ++
     }
     // console.log("64Guess_arr in gus_counts: ", guess_count_array);
@@ -99,7 +95,6 @@ function get_well_placed(guess) {
     console.log("71secret: ", secret)
     console.log("72guess: ", guess)
     for (i = 0; i < 4; i++) {
-        // console.log("74Sec[i]Gus[i]: ", secret[i], guess[i])
         if (secret[i] == guess[i]) {
             well_placed ++;
         }
@@ -125,8 +120,6 @@ function get_misplaced(secr_count_array, guess_count_array, well_placed) {
 }
 
 function submit_guess(guess) {
-    // let guess = document.getElementById("guess").value;
-
     let is_val_guess = validate_guess(guess);
     if (is_val_guess != 1) {
         console.log("Wrong input! Guess needs to be 4 numbers between 0 - 7")
@@ -134,30 +127,35 @@ function submit_guess(guess) {
     else {
         console.log("115Guess Valid")
         well_placed = get_well_placed(guess)
-            if (well_placed == 4) {
-                console.log("You won!")
-                you_won = 1;
+
+        if (well_placed == 4) {
+            console.log("You won!")
+            you_won = 1;
+            gridFour.classList.add("show-grid-con-4")
+            // gridFour.style.display = "block !important";
+            gridThree.style.display = "none";
+            console.log("133: ", gridThree);
             
-                return;
-            }
+            console.log("135: ", gridFour);
+            return;
+        }
+        // gridThree.style.display = "block";
+        // console.log("139: ", gridThree);
+        // gridFour.style.display = "none";
+        // console.log("141: ", youWon);
         let guess_count_array = guess_counts(guess)
         misplaced = get_misplaced(secr_count_array, guess_count_array, well_placed)
-
         misPlaced.innerText = misplaced;
         wellPlaced.innerText = well_placed;
+
     }
 }
 
+
 // Play a game round
 function play_round() {
-    // console.log("Roundnum <= max: ", round_num <= max_tries);
-    // console.log("You_won: ", you_won);
     if (round_num <= max_tries && you_won == 0) {
-
         console.log("Round #", round_num)
-
-        // const prompt = require('prompt-sync')();
-        // let guess = prompt("Please enter a 4 digit guess: ")
         let guess = guessInput.value 
         console.log ("155guess from input: ", guess)
         round_complete = 0;
@@ -170,7 +168,6 @@ function play_round() {
         console.log("140Roundnum <= max: ", round_num <= max_tries);
         console.log("141You_won: ", you_won);
         guessInput.value = null // or ""
-
     }
     if (round_num > max_tries) {
         console.log("Sorry, too many tries. The code was ", secret)
@@ -180,32 +177,9 @@ function play_round() {
 
 // Play the game
 function play_game() {
-    // round_num = 1; 
-    // tries = 10
+    gridFour.style.display = "none !important";
     make_secret()
     secret_counts()
+
     play_round()
 }
-
-// Run the program; make the secret and count array, then play
-
-
-// function startGame() {
-//     mmind()
-// }
-
-
-// play game
-// make secret
-// make secret_count_array
-
-// play round:
-//  submit guess
-//  valdidate guess
-//      if not valid, error
-//      if valid, compare for well placed matches
-//          if 4, won!
-//          else, make guess_count_array
-//  output well and misplaced
-//  add to round number and play again
-// 
